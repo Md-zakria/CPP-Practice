96bb848ac7c459a08e3b24a5ffcd41aafac74432
Muhammad Zakria <zakriajutt2917@gmail.com>
2026-04-21 01:51:43 +0500

    Add basic input/output example in C++

---
+++ 01_Basics/input_output.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // store the number user types
+    int number;
+
+    // read the number from user
+    cin >> number;
+
+    // print it back
+    cout << number << endl;
+
+    return 0;
+}
